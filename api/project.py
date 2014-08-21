16cae9e4682cecbe177a335c9860bdc3d6f84f18
hitigon <hitigon@gmail.com>
2014-08-20 23:39:58 -0700

    add project PUT method

--- api/project.py
+++ api/project.py
@@ -112,26 +112,57 @@ class ProjectHandler(BaseHandler):
         tags = self.get_argument('tags', None)
 
         user = kwargs['user']
+        path = parse_path(args[0])
+        project = Project.objects(name=path[0]).first()
+        if project and user not in project.members:
+            self.raise401()
+        project_leader = project.leader
         update = {}
         if name:
-            update['name'] = name
+            update['set__name'] = name
         if description:
-            update['description'] = description
+            update['set__description'] = description
         if url:
-            update['url'] = url
+            update['set__url'] = url
         if leader:
-            update['leader'] = leader
+            project_leader = User.objects(username=leader).first()
+            update['set__leader'] = project_leader
         if members:
-            update['members'] = members
+            members_list = []
+            for member in parse_listed_strs(members):
+                u = User.objects(username=member).first()
+                if not u or u == project_leader:
+                    continue
+                members_list.append(u)
+            members_list.append(project_leader)
+            update['set__members'] = members_list
         if teams:
-            update['teams'] = teams
+            teams_list = []
+            for team in parse_listed_strs(teams):
+                t = Team.objects(name=team).first()
+                if not t:
+                    continue
+                teams_list.append(t)
+            update['set__teams'] = teams_list
         if repos:
-            update['repos'] = repos
+            repos_list = []
+            for repo in parse_listed_strs(repos):
+                r = Repo.objects(name=repo).first()
+                if not r:
+                    continue
+                repos_list.append(r)
+            update['set__repos'] = repos_list
         if tags:
             tags_list = parse_listed_strs(tags)
-            update['tags'] = tags_list
-        response = {}
-        self.write(response)
+            update['set__tags'] = tags_list
+        try:
+            Project.objects(name=path[0]).update_one(**update)
+            project = Repo.objects(name=name or path[0]).first()
+            self.set_status(201)
+            self.write(convert_document(project))
+        except Exception as e:
+            reason = e.message
+            self.raise400(reason=reason)
 
     @authenticated(scopes=['projects'])
     def delete(self, *args, **kwargs):
