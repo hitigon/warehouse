60036088fd4da3cc213eef21b204b3403f6124df
hitigon <hitigon@gmail.com>
2014-08-21 13:33:02 -0700

    remove old libs and models

--- libs/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-#
-# @name: libs/__init__.py
-# @create: Apr. 27th, 2014
-# @author: hitigon@gmail.com
--- libs/morm/model.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-#
-# @name: lib/morm/document.py
-# @date: Aug. 7th, 2014
-# @author: hitigon@gmail.com
